2d9ca5d680f72bef7ec1a364a3055c4f9cb79c3b
Your Name <you@example.com>
2023-06-16 17:05:45 +0900

    이전달 다음달 & 헤드넘김

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,40 @@ function App() {
 
   console.log(year, month, date);
 
+
+  //다음 달
+  const nextMonth = () => {
+    if(month == 11){
+      setMonth(0);//1월
+      setYear(year + 1) //다음 년도
+      return;
+    }
+    setMonth(month + 1);
+  
+  }
+  console.log('month',month)
+
+  //이전 달
+  const prevMonth = () => {
+    if(month == 0){
+      setMonth(11);
+      setYear(year -1);//이전 년도 
+      return;
+    }
+    setMonth(month - 1);
+   
+  }
+
   return (
     <div className='App'>
       <Header />
       {/* 년-월-일 state를 Calendar 컴포넌트에 전달 */}
       <Calendar year={year} month={month} date={date} /> {/* 컴포넌트 이름 수정 */}
+      <div className='btn-group'>
+        <button className='prev' onClick={prevMonth}>이전달</button>
+        <button className='next'onClick={nextMonth}>다음달</button>
+
+      </div>
     </div>
   )
 }
--- src/components/Calender.jsx
+++ src/components/Calender.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 
 function Calendar(props) {
-  // props에서 년-월-일 가져오기
-  let { year, month, date } = props;
-
+  //props에서 년-월-일 가져오기
+  let {year,month,date} = props;
+  
+  
   // 날짜 조회하기
-  // let d = new Date();
+  let d = new Date();
   // let year = d.getFullYear();  // 년도
   // let month = d.getMonth();    // 월 
   // let date = d.getDate();      // 일
   // console.log(year, month, date);
 
   // 말일 구하기(다음달 기준으로 조회해서 일값=0 하면 이번달 말임)
-  let lastDate = new Date(year, month + 1, 0).getDate();
-  // 시작일: 달력에서 1일이 시작할 위치 (요일값 = 0....6)
-  let start_date = new Date(year, month, 1).getDay();
-  console.log(lastDate, start_date);
+  let lastDate = new Date(year,month + 1, 0).getDate();
+//시작일: 달력에서 1일이 시작할 위치 (요일값 =0....6)
+let start_date = new Date(year,month, 1).getDay()
+console.log(lastDate,start_date);
 
   return (
     <div className='calendar'>
@@ -25,22 +26,26 @@ function Calendar(props) {
       </header>
       <main>
         <ul className='date'>
-          {Array(start_date).fill().map((_, i) => {
-            return <li key={i}></li>;
-          })}
-          {Array(lastDate).fill().map((_, i) => {
-            return (
-              <li
-                key={i}
+          {
+            Array(start_date).fill().map((_, i) =>{
+                return (
+                  <li key={i}></li>
+                )
+            })
+          }
+          {
+            Array(lastDate).fill().map((_, i )=>{
+              return(
+              <li key={i}
                 style={{
-                  color: date === i + 1 ? 'red' : 'white',
-                  borderRadius: date === i + 1 ? '10px' : '0',
+                  color:date === i +1 ? 'red' : 'white',
+                  borderRadius: date === i ? '10px' : '0',
                 }}
-              >
-                {i + 1}
-              </li>
-            );
-          })}
+                >{i +1}</li>
+              )
+            })
+          }
+
         </ul>
       </main>
     </div>
